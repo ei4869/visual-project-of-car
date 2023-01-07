4684e7306b848eeff69f92ac3f0d3682cc68a911
4869 <1011606059@qq.com>
2023-01-07 20:02:37 +0800

    更新

--- Car_project/function.py
+++ Car_project/function.py
@@ -3,21 +3,16 @@ import numpy as np
 import os
 import time
 #import serial
-
-
 WIDTH = 640
 HEIGHT = 480
+
+DIRECTION = 'R'
 
 capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
-capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-#ser = serial.Serial("/dev/ttyAMA0", 115200)
+#capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 
-PATH1 = "WaitStop/"
-PATH2 = "Stop/"
-SVMNAME = "test1"
-TRAIN_NUM = 30
 COLOR_DICT = {
                 'highred':[np.array([156,43,46]),np.array([180,255,255])],
                 'lowred':[np.array([0,43,46]),np.array([10,255,255])],
@@ -27,113 +22,152 @@ COLOR_DICT = {
                 'orange':[np.array([11,43,46]),np.array([25,255,255])]}
 COLORS = ['highred','lowred','green','blue','yellow','orange']
 
-#1 识别等停和停止 SVM二值回归
-class WaitandStop():
-    def __init__(self):
-        self.path1 = PATH1
-        self.path2 = PATH2
-        self.num = TRAIN_NUM
-        self.savename = "svm{}.mat".format(SVMNAME)
-        self.svm = None
-        self.labels = None
-        self.samples = None
-        #裁剪的ROI参数
-        self.cuty0 = HEIGHT*2//5
-        self.cuty1 = HEIGHT*4//5
-        self.cutx0 = WIDTH//3
-        self.cutx1 = WIDTH*2//3
-    #加载数据集并添加标签
-    def data_load(self,path,num,act_label,samples,labels):
-        for i in range(1,num):
-            src_img=cv2.imread(path+str(i)+'.jpg')
-            gray_img=cv2.cvtColor(src_img,cv2.COLOR_BGR2GRAY)
-            resize_img=cv2.resize(gray_img,(20,20))
-            temp=np.resize(resize_img,(1,20*20))
-            description=temp.astype(np.float32)
-            samples.append(description)
-            labels.append(act_label)
-    #图像预处理
-    def img_pre_process(self,image):
-        gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        resize_img = cv2.resize(gray_img, (20, 20))
-        temp = np.resize(resize_img, (1, 20 * 20))
-        description=temp.astype(np.float32)
-        return description
-    #裁剪目标区域
-    def cut_img(self,image):
-        cut_image=image[self.cuty0:self.cuty1,self.cutx0:self.cutx1]    #裁剪ROI
-        cv2.imshow('1',cut_image)
-        return cut_image
-    #圈出轮廓矩形
-    def get_rectangle(self,image):   
-        #圈出最大轮廓
-        gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+def nms(dets, nmsThreshold):
+    """
+    非极大值抑制
+    dets: [[x1, y1, x2, y2, score], [x1, y1, x2, y2, score], ...]
+    """
+    # dets:N*M,N是bbox的个数，M的前4位是对应的（x1,y1,x2,y2），第5位是对应的分数
+    # #thresh:0.3,0.5....
+    if dets.shape[0] == 0:
+        return []
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    scores = dets[:, 4]
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)  # 求每个bbox的面积
+    order = scores.argsort()[::-1]  # 对分数进行倒排序
+    keep = []  # 用来保存最后留下来的bboxx下标
+    while order.size > 0:
+        i = order[0]  # 无条件保留每次迭代中置信度最高的bbox
+        keep.append(i)
+        # 计算置信度最高的bbox和其他剩下bbox之间的交叉区域
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+        # 计算置信度高的bbox和其他剩下bbox之间交叉区域的面积
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        # 求交叉区域的面积占两者（置信度高的bbox和其他bbox）面积和的比例
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+        # 保留ovr小于thresh的bbox，进入下一次迭代。
+        inds = np.where(ovr <= nmsThreshold)[0]
+        # 因为ovr中的索引不包括order[0]所以要向后移动一位
+        order = order[inds + 1]
+    output = []
+    for i in keep:
+        output.append(dets[i].tolist())
+    return output
 
-        binary=cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,5,4)
 
-        contours = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0]
-        t=()
-        
-        t = max(contours,key=cv2.contourArea)
-        cv2.drawContours(image,t,-1,(0,255,0))
-        cv2.imshow('1',binary)
-        x, y, w, h = cv2.boundingRect(t)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
-        return x,y,w,h
-    #对获取的帧的ROI进行分类
-    def train(self):
-        if os.path.exists(self.savename):
-            try:
-                self.svm = cv2.ml.SVM_load(self.savename)
-                print("模型加载成功!")
-            except Exception:
-                print("导入失败!")
-                exit()
-        else:
-            #创建一个SVM分类器
-            self.svm=cv2.ml.SVM_create()
-            #参数设置
-            self.svm.setType(cv2.ml.SVM_C_SVC)
-            self.svm.setKernel(cv2.ml.SVM_LINEAR)
-            self.svm.setP(0.1)
-            criterial = (cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-6)
-            self.svm.setTermCriteria(criterial)
-            #数据集创建
-            samples=[]
-            labels=[]
-            #加载数据集
-            self.data_load(self.path1,self.num,0,samples,labels)
-            self.data_load(self.path2,self.num,1,samples,labels)
-            #将原始数据集（列表形式）转化为numpy数组形式，且需要是float32类型，因为SVM只接受这种类型
-            samples_number = len(samples)
-            samples = np.float32(samples)
-            samples = np.resize(samples, (samples_number, 20*20))
-            labels = np.int32(labels)
-            labels = np.resize(labels, (samples_number, 1))
-            print('training\n')
-            #开始训练
-            self.svm.trainAuto(samples,cv2.ml.ROW_SAMPLE, labels)
-            self.svm.save(self.savename)
-            print('ok')
-            self.labels =labels
-            self.samples = samples
-    def start(self,frame):
-        cut0=self.cut_img(frame)#初步裁剪出一个较小的区域
-        try:
-            x,y,w,h=self.get_rectangle(cut0)#找出框住字符的矩形
-            cv2.rectangle(frame, (x+WIDTH//3, y+HEIGHT*2//5), (x + w+WIDTH//3, y + h+HEIGHT*2//5), (0, 0, 255), 3)#在最原始的图中绘制找到的矩形框
-            cut1=cut0[y:y+h,x:x+w]#按找到的矩形裁剪cut0得到最终的字符
-            test=self.img_pre_process(cut1)#将得到的图片按处理数据集的方法处理得到test
-            result=self.svm.predict(test)#将test给SVM分类器预测
-            if result[1][0][0]==0.0:
-                print('WaitStop')
-            elif result[1][0][0] == 1.0:
-                print('Stop')
 
-            cv2.imshow('video', frame)
-        except Exception:
-            pass
-#############################################################################################  
+def tanh(x):
+    return 2.0 / (1 + np.exp(-2 * x)) - 1
+
+
+def draw_pred(frame, class_name, conf, left, top, right, bottom):
+    """
+    绘制预测结果
+    """
+    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), thickness=2)
+    label = f"{class_name}: {conf:.2f}"
+    labelSize, _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.8, 2)
+    top = max(top - 10, labelSize[1])
+    left = max(left, 0)
+    cv2.putText(
+        frame,
+        label,
+        (left, top),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.8,
+        (0, 255, 0),
+        thickness=2,
+    )
+class FastestDet:
+    def __init__(self, confThreshold=0.5, nmsThreshold=0.4, drawOutput=False):
+        """
+        FastestDet 目标检测网络
+        confThreshold: 置信度阈值
+        nmsThreshold: 非极大值抑制阈值
+        """
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "models")
+        path_names = os.path.join(path, "C:/Users/4869/Desktop/Car_project/SolutionsNew/train10/FastestDet.names")  # 识别类别
+        path_onnx = os.path.join(path, "C:/Users/4869/Desktop/Car_project/SolutionsNew/train10/FastestDet.onnx")
+        self.classes = list(map(lambda x: x.strip(), open(path_names, "r").readlines()))
+        self.inpWidth = 150
+        self.inpHeight = 150
+        self.net = cv2.dnn.readNet(path_onnx)
+        self.confThreshold = confThreshold
+        self.nmsThreshold = nmsThreshold
+        self.drawOutput = drawOutput
+
+    def post_process(self, frame, outs):
+        """
+        后处理, 对输出进行筛选
+        """
+        outs = outs.transpose(1, 2, 0)  # 将维度调整为 (H, W, C)
+        frameHeight = frame.shape[0]
+        frameWidth = frame.shape[1]
+        feature_height = outs.shape[0]
+        feature_width = outs.shape[1]
+        preds = []
+        confidences = []
+        boxes = []
+        ret = []
+        for h in range(feature_height):
+            for w in range(feature_width):
+                data = outs[h][w]
+                obj_score, cls_score = data[0], data[5:].max()
+                score = (obj_score**0.6) * (cls_score**0.4)
+                if score > self.confThreshold:
+                    classId = np.argmax(data[5:])
+                    # 检测框中心点偏移
+                    x_offset, y_offset = tanh(data[1]), tanh(data[2])
+                    # 检测框归一化后的宽高
+                    box_width, box_height = sigmoid(data[3]), sigmoid(data[4])
+                    # 检测框归一化后中心点
+                    box_cx = (w + x_offset) / feature_width
+                    box_cy = (h + y_offset) / feature_height
+                    x1, y1 = box_cx - 0.5 * box_width, box_cy - 0.5 * box_height
+                    x2, y2 = box_cx + 0.5 * box_width, box_cy + 0.5 * box_height
+                    x1, y1, x2, y2 = (
+                        int(x1 * frameWidth),
+                        int(y1 * frameHeight),
+                        int(x2 * frameWidth),
+                        int(y2 * frameHeight),
+                    )
+                    preds.append([x1, y1, x2, y2, score, classId])
+                    boxes.append([x1, y1, x2 - x1, y2 - y1])
+                    confidences.append(score)
+        indices = cv2.dnn.NMSBoxes(
+            boxes, confidences, self.confThreshold, self.nmsThreshold
+        )
+        indices = np.array(indices).flatten().tolist()
+        for i in indices:
+            pred = preds[i]
+            score, classId = pred[4], int(pred[5])
+            x1, y1, x2, y2 = pred[0], pred[1], pred[2], pred[3]
+            center_x, center_y = (x1 + x2) / 2, (y1 + y2) / 2
+            ret.append(((center_x, center_y), self.classes[classId], score))
+            if self.drawOutput:
+                draw_pred(frame, self.classes[classId], score, x1, y1, x2, y2)
+        return ret
+
+    def detect(self, frame):
+        """
+        执行识别
+        return: 识别结果列表: (中点坐标, 类型名称, 置信度)
+        """
+        blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (self.inpWidth, self.inpHeight))
+        self.net.setInput(blob)
+        pred = self.net.forward(self.net.getUnconnectedOutLayersNames())[0][0]
+        return self.post_process(frame, pred)
 #识别交通灯颜色    
 class ColorRecognize():
     def __init__(self):
@@ -194,17 +228,18 @@ class ColorRecognize():
 #############################################################################################
 #巡线
 class Findroute():
-    def __init__(self,direction = 'R'):
+   
+    def __init__(self,direct):
         #self.RouteRegionParams = [HEIGHT//2+130,HEIGHT//2+150,WIDTH//12+20,WIDTH-WIDTH//12-20]
         self.RouteRegionParams = [HEIGHT//2+130,HEIGHT//2+150,WIDTH//12+30,WIDTH-WIDTH//12-10]
         self.UpWaitstopRegionParams = [HEIGHT//2+60,HEIGHT-160,WIDTH//3-50,WIDTH*2//3+50]
         self.DownWaitstopRegionParams = [HEIGHT//2,HEIGHT//2+60,WIDTH//3-50,WIDTH*2//3+50]
         self.threshold = 130
-        self.direction = direction
-        pass
+        self.direction = direct
     def start(self,img):
         try:
-        #转灰度图
+            direction = self.direction
+            #转灰度图
             Gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             [cropx1,cropx2,cropy1,cropy2] = self.RouteRegionParams   #裁剪的ROI参数
         
@@ -219,25 +254,10 @@ class Findroute():
             Routeregion = cv2.morphologyEx(Routeregion, cv2.MORPH_OPEN, kernel,iterations=1)
             Routeregion = cv2.morphologyEx(Routeregion, cv2.MORPH_CLOSE, kernel,iterations=1)
             
-            #框出等停标志识别区域
-            UpWaitstopRegion = Gray[self.UpWaitstopRegionParams[0]:self.UpWaitstopRegionParams[1],self.UpWaitstopRegionParams[2]:self.UpWaitstopRegionParams[3]]
-            DownWaitstopRegion = Gray[self.DownWaitstopRegionParams[0]:self.DownWaitstopRegionParams[1],self.DownWaitstopRegionParams[2]:self.DownWaitstopRegionParams[3]]
-            white = len(np.where(UpWaitstopRegion == 255)[0])
-            black = len(np.where(DownWaitstopRegion == 0)[0])
-            size1 = UpWaitstopRegion.size
-            size2 = DownWaitstopRegion.size
-            white_ratio = white/size1*100
-            black_ratio = black/size2*100
-            # if black_ratio>=90 and white_ratio>=20:
-                # ser.write("w\r\n".encode('utf-8'))
-            #print('bt,wt',black_ratio,white_ratio)
-            # cv2.imshow('2',UpWaitstopRegion)
-            # cv2.imshow('3',DownWaitstopRegion)
-
             #寻找ROI中黑线轮廓
             contours = cv2.findContours(Routeregion, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[0]
             delta = np.array([[cropy1,cropx1]]) #变换坐标的参数(裁切后的图的坐标转化为原灰度图坐标)
-            
+            print(self.direction)
             if self.direction == "R":
                 contour = contours[1]   #与左右转弯有关，这是参考的轮廓
             elif self.direction == "L":
@@ -261,41 +281,39 @@ class Findroute():
                 if direction >= 150:
                     direction = 150
                 #ser.write('#{}#\r\n'.format(int(direction)).encode('utf-8'))
-                print("右转")
+                #print("右转")
             elif direction < 0:
                 if direction <= -150:
                     direction = -150
                 #ser.write('#{}#\r\n'.format(int(direction)).encode('utf-8'))
-                print('左转')
-            print('juli',direction)
+                #print('左转')
+            #print('juli',direction)
             
-            #截取出停止标志识别区域
-            StopRegion = Gray[y+h//2-200:HEIGHT-250,x+w+10-50:x+w+10+50]
-
-            white_pixels = len(np.where(StopRegion == 255)[0])
-            pixels = StopRegion.size
-            white_ratio = white_pixels/pixels*100
-            if white_ratio>=50 and white_ratio<=65:
-                #ser.write("s\r\n".encode('utf-8'))
-                pass
-            print('stopwt',white_ratio)
+            get = deep.detect(img)[0][1]
+            if get == 'waitstop':
+                #ser.write('w\r\n'.encode('utf-8'))
+                print("等停")
+            elif get == 'stop':
+                #ser.write('s\r\n'.encode('utf-8'))
+                print("停止")
+            elif get == 'L':
+                direction = 'L'
+            elif get == 'R':
+                direction = 'R'
+            self.direction = direction
             
-            cv2.rectangle(img,(cropy1,cropx1),(cropy2,cropx2),(0,255,0),3)
-            cv2.imshow('1',img) 
-            time.sleep(0.04)
         except Exception:
             pass
+        cv2.rectangle(img,(cropy1,cropx1),(cropy2,cropx2),(0,255,0),3)
+        cv2.imshow('1',img) 
 #############################################################################################
 
-A = WaitandStop()
-# B = ColorRecognize()
-#C = Findroute()
-A.train()
+deep = FastestDet(drawOutput=True)
+#B = ColorRecognize()
+C = Findroute(direct='R')
 while True:
     ret, frame = capture.read()
-    A.start(frame)
-    #B.start(frame)
-    #C.start(frame)
+    C.start(frame)
     if cv2.waitKey(10) == 27:
         break
 cv2.destroyAllWindows() 
